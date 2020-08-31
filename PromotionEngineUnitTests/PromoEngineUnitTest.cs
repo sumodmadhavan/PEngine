f53ba6e6ec8cadb4dda098318cadd5765cef929b
Sumod Madhavan <sumadh@microsoft.com>
2020-08-31 22:37:30 +0530

    building the skeleton for test data

---
+++ PromotionEngineUnitTests/PromoEngineUnitTest.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using PromoEngineLibrary.Entity;
+using PromoEngineLibrary;
+namespace PromotionEngineUnitTests
+{
+    [TestFixture]
+    public class PromotionEngineUnitTests
+    {
+
+        /*
+        Scenario A
+         1 * A 50
+         1 * B 30
+         1 * C 20
+         ========
+         Total 100
+         */
+        [Test]
+        public void SKU_One_Each_ScenarioA()
+        {
+            //Assign
+            
+            //Act
+            
+
+            //Assert
+            
+
+        }
+        /*
+        Scenario B
+         5 * A 130 + 2 * 50
+         5 * B 45 + 45  + 30
+         1 * C 20
+         ========
+         Total 370
+         */
+
+        [Test]
+        public void SKU_Offer_ScenarioB()
+        {
+            //Assign
+            
+            //Act
+            
+
+            //Assert
+            
+
+        }
+        /*
+        Scenario C
+        3 * A 130
+        5 * B 45 + 45 +1 * 30
+        1 * C -
+        1 * D 30
+        ========
+        Total 370
+        */
+       /[Test]
+        public void SKU_Offer_ScenarioC()
+        {
+            //Assign
+            
+
+            //Act
+            
+
+            //Assert
+            
+
+        }
+    }
+}
