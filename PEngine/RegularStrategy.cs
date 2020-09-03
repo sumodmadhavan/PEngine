0c388f27546e6f92444e6e5507f6bf368865510d
Sumod Madhavan <sumadh@microsoft.com>
2020-09-03 19:23:49 +0530

    adding strategy

---
+++ PEngine/RegularStrategy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace PEngine
+{
+    public abstract class RegularStrategy : IPricingStrategy
+    {
+        public abstract Sku Sku { get; }
+        protected abstract double Price { get; }
+
+        public double GetPrice(int count)
+        {
+            return Price * count;
+        }
+    }
+
+    public class PricingStrategyC : RegularStrategy
+    {
+        public override Sku Sku { get; } = 'C';
+        protected override double Price { get; } = 20;
+    }
+
+    public class PricingStrategyD : RegularStrategy
+    {
+        public override Sku Sku { get; } = 'D';
+        protected override double Price { get; } = 15;
+    }
+}
---
+++ PEngine/SaleStrategy.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace PEngine
+{
+    public abstract class SaleStrategy : IPricingStrategy
+    {
+        public abstract Sku Sku { get; }
+        protected abstract double PricePerOne { get; }
+
+        //protected abstract double PricePerCandD { get; }
+        protected abstract double PricePerX { get; }
+        protected abstract int X { get; }
+
+        public double GetPrice(int count)
+        {
+            if (count == 0)
+            {
+                return 0;
+            }
+
+            double result = 0;
+
+            if (Sku == '&')
+            {
+                return PricePerX;
+            }
+            else
+            {
+                while (count >= X)
+                {
+                    result = result + PricePerX;
+                    count = count - X;
+                }
+            }
+
+            return result + (PricePerOne * count);
+        }
+    }
+}
